d50376a7d6ae3f4e5a6df610622d959db2a8aa58
Marius WIN11 <marius.ledig@epitech.eu>
2021-10-07 19:39:14 +0200

    fixed android compilation and modify app name

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "SmartSoccer",
+      title: "GamblingApp",
       theme: ThemeData(
           primaryColor: const Color(0xFF202020),
           scaffoldBackgroundColor: const Color(0xFF171819),
