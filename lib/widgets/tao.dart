b1d9e210597b79e06e79de3ffd36cd4feed790d9
Taotao <yuantao.li@epitech.eu>
2021-10-16 20:36:35 +0800

    choice your image

--- lib/widgets/tao.dart
+++ lib/widgets/tao.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
@@ -19,10 +21,75 @@ void rule_tips() {
 
 class _TaoWidgetState extends State<TaoWidget> {
 
+  late File imageFile;
+
+  _openGallary(BuildContext context) async {
+    final ImagePicker _picker = ImagePicker();
+    var picture = await _picker.pickImage(source: ImageSource.gallery);
+    this.setState(() {
+      if (picture != null) {
+        imageFile = File(picture.path);
+      } else {
+        print("No image Selected");
+      }
+    });
+    Navigator.of(context).pop();
+  }
+
+  _openCamera(BuildContext context) async {
+    final ImagePicker _picker = ImagePicker();
+    var picture = await _picker.pickImage(source: ImageSource.camera);
+    this.setState(() {
+      if (picture != null) {
+        imageFile = File(picture.path);
+      } else {
+        print("No image Selected");
+      }
+    });
+    Navigator.of(context).pop();
+  }
+
+  Future<void> _showChoiceDialog(BuildContext context) {
+    return showDialog(context: context,builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text("Make a choice"),
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: [
+              GestureDetector(
+                child: Text("Gallary"),
+                onTap: () {
+                  _openGallary(context);
+                },
+              ),
+              Padding(padding: EdgeInsets.all(8.0)),
+              GestureDetector(
+                child: Text("Camera"),
+                onTap: () {
+                  _openCamera(context);
+                },
+              )
+            ],
+          ),
+        ),
+      );
+    });
+  }
+
+  Widget _decideImageView() {
+    if (imageFile == null) {
+      return Text("No image choice");
+    } else {
+      return Image.file(imageFile, width: 300, height: 300,);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Row(
             children: [
@@ -34,31 +101,35 @@ class _TaoWidgetState extends State<TaoWidget> {
             ],
           ),
           SizedBox(height: 50,),
-          Container(
-            color: Colors.blue,
-            width: 300,
-            height: 300,
-            // decoration: BoxDecoration(
-            //   color: Colors.white,
-            //   borderRadius: BorderRadius.circular(150),
-            // ),
-            child: Image(image: AssetImage("assets/user.png")
-            ),
+          // Container(
+          //   color: Colors.blue,
+          //   child: Image.asset("assets/user.png", fit: BoxFit.cover,),
+          //   width: 300,
+          //   height: 300,
+          // ),
+          SizedBox(height: 10,),
+          Row(
+            children: [
+              // _decideImageView(),
+              RaisedButton(onPressed: () {
+                _showChoiceDialog(context);
+              }, child: Text("Select Image"),)
+            ],
           ),
           SizedBox(height: 70,),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [
+            children: const [
               Text("Gold Coins", style: TextStyle(color: Colors.white),),
               SizedBox(width: 50),
               TextButton(
                 onPressed: _Statistics,
-                child: const Text("Statistics"),
+                child: Text("Statistics"),
               ),
               SizedBox(width: 50),
               TextButton(
                 onPressed: rule_tips,
-                child: const Text("Rules and Tips"),
+                child: Text("Rules and Tips"),
               ),
             ],
           )
